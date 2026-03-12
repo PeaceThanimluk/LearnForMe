ad93a51b034e157c1c5612372ac653faeffb7689
Supatcharanan <supatcharanan.thanimluk@gmail.com>
2026-03-13 00:28:10 +0700

    Update Constructor.py

--- Python/oop1/Constructor/Constructor.py
+++ Python/oop1/Constructor/Constructor.py
@@ -3,3 +3,19 @@
 def__init__(self):
     pass
 '''
+
+class Employee:
+
+    def __init__(self , name , salary): 
+        self.name = name
+        self.salary = salary
+    
+    def ShowData(self): #Function use to show data
+        print("Name = {}".format(self.name))
+        print("Salary = {}".format(self.salary))
+    
+
+obj1 = Employee("peace" , 10)
+obj2 = Employee("monk" , 100)
+obj1.ShowData()
+obj2.ShowData()
