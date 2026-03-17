a1373b258ef9d8f08f768a763199a299edf65565
Supatcharanan <supatcharanan.thanimluk@gmail.com>
2026-03-18 03:43:11 +0700

    create sty.py file

---
+++ Python/Learn/OOP/Str/Str.py
@@ -0,0 +1,58 @@
+#แปลงobjเป็นstring 
+"""
+def__str__(self):
+    return "ชุดข้อความ"
+"""
+
+class Employee: #SuperClass
+    #Variables
+    MinSalary = 12000
+    MaxSaraly = 50000
+    _CompanyName = "SixSevenCompany"
+    __Private = "Baka"
+
+
+    def __init__(self , name , salary , Department): 
+       #Instance Variable
+        self.__name = name
+        self.__salary = salary
+        self._Department = Department
+
+    
+    def ShowData(self): #Function use to show data
+        print("Name = {}".format(self.__name))
+        print("Salary = {}".format(self.__salary))
+
+    #รายได้ต่อปี
+    def _getIncome(self):
+        return self.__salary * 12
+
+    def __str__(self):
+        return(f"ชื่อพนักงาน : {self.__name} , แผนก: {self._Department} , เงินเดือน: {self.__salary} , รายได้ต่อปี: {self._getIncome}")
+
+#SubClass
+class Accounting(Employee): #Inheritance
+    __DepartmentName = "บัญชี"
+    def __init__(self , name , salary): #นำค่านำเข้าก่อน
+        super().__init__(name , salary , self.__DepartmentName) #นำค่าที่ได้จากInitของAccountไปใส่ของEmployee
+
+class Programmer(Employee):
+    __DepartmentName = "โปรแกรมเมอร์"
+    def __init__(self , name , salary):
+        super().__init__(name , salary , self.__DepartmentName) #ใช้กับConstructor
+        self.ShowData()
+
+
+class Sale(Employee):
+    __DepartmentName = "ขาย"
+    def __init__(self , name , salary):
+        super().__init__(name , salary , self.__DepartmentName)
+
+
+#crate object
+Account = Accounting("FreeFire" , 20000)
+programmer = Programmer("King" , 60000)
+
+
+#Show data
+print(Account.__str__())
