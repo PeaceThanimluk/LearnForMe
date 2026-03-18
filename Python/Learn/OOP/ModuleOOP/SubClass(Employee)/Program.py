798b19f90e8ba3b5cfe0d2c8d0d5d0a822778abd
Supatcharanan <supatcharanan.thanimluk@gmail.com>
2026-03-18 18:11:17 +0700

    Module oop

---
+++ Python/Learn/OOP/ModuleOOP/SubClass(Employee)/Program.py
@@ -0,0 +1,14 @@
+#ต้องอยู่ในfolderเดียวกันถึงจะimportได้
+from Account import Accounting #from ชื่อไฟล์ import ชื่อMethod
+from Programmer import Programmer
+from Sale import Sale 
+#crate object
+Account = Accounting("FreeFire" , 20000 , 30)
+programmer = Programmer("King" , 60000 , 2 , "Coding")
+Sale = Sale("Baka" , 20000 , "Bangkok")
+
+
+#Show data
+# Account.ShowData()
+
+print(f"MOney: {Account._getIncome(100)}")
