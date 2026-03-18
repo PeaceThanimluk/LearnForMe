d8e2ada9e32ebf5e8932bc1728d75dd5dc57ef9f
Supatcharanan <supatcharanan.thanimluk@gmail.com>
2026-03-18 18:21:45 +0700

    change

--- Python/Learn/OOP/ModuleOOP/SubClass(Employee)/Program.py
+++ Python/Learn/OOP/ModuleOOP/SubClass(Employee)/Program.py
@@ -1,14 +1,13 @@
 #ต้องอยู่ในfolderเดียวกันถึงจะimportได้
-from Account import Accounting #from ชื่อไฟล์ import ชื่อMethod
+# from account import Accounting #from ชื่อไฟล์ import ชื่อMethod
 from programmer import Programmer
 from sale import Sale 
 #crate object
-Account = Accounting("FreeFire" , 20000 , 30)
-programmer = Programmer("King" , 60000 , 2 , "Coding")
-Sale = Sale("Baka" , 20000 , "Bangkok")
+
+programmer1 = Programmer("King" , 60000 , 2 , "Coding")
+Sale1 = Sale("Baka" , 20000 , "Bangkok")
 
 
 #Show data
 # Account.ShowData()
 
-print(f"MOney: {Account._getIncome(100)}")
