a6200a4d38421046c12406fabc578f221a4bb8ba
Supatcharanan <supatcharanan.thanimluk@gmail.com>
2026-03-21 05:16:30 +0700

    Composition Homework(not finish yet)

---
+++ Python/Learn/OOP/็Homework/Composition/PlayerSystem/Weapon.py
@@ -0,0 +1,9 @@
+from Health import health
+
+class weapon:
+    def __init__(self , name , damage):
+        self.name = name
+        self.damage = damage
+
+    def attack(self , enemy):
+        pass
