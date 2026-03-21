94ef0949fb0c1ea05f096660d5f978dd993225c5
Supatcharanan <supatcharanan.thanimluk@gmail.com>
2026-03-21 19:24:04 +0700

    delete file

--- Python/Learn/OOP/็Homework/Composition/PlayerSystem/Weapon.py
+++
@@ -1,9 +0,0 @@
-from Health import health
-
-class weapon:
-    def __init__(self , name , damage):
-        self.name = name
-        self.damage = damage
-
-    def attack(self , enemy):
-        pass
