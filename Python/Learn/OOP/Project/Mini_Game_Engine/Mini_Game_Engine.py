6564ac26dcba6eb8956739ecf29b1e0bf9e2bf03
Supatcharanan <supatcharanan.thanimluk@gmail.com>
2026-03-28 19:54:48 +0700

    Update Project
    สร้างเค้าโครงไว้ทำต่อ

--- Python/Learn/OOP/Project/Mini_Game_Engine/Mini_Game_Engine.py
+++ Python/Learn/OOP/Project/Mini_Game_Engine/Mini_Game_Engine.py
@@ -1,12 +1,87 @@
-class Player:
-    def __init__(self , name , hp , attack):
-        self.name = name
-        self.hp = hp
-        self.attack = attack
-        self.inventory = []
+from abc import ABC , abstractmethod
 
-class Enenmy:
-    def __init__(self , name , hp , attack):
+# ===================================
+# CHATACTER CLASS
+# ===================================
+
+class Character(ABC):
+
+    def __init__(self , name: str , hp: int , mp : int , attack : int , defense : int):
+        #defalut_attribute
         self.name = name
+        self.max_hp = hp
+        self.max_mp = mp
+        self.level = 1
+        self.exp = 0
+        self.Inventory = Inventory() #Class
+        #attribute
         self.hp = hp
+        self.mp = mp
         self.attack = attack
+        self.defense = defense
+        #List_attribute
+        self.skill:list["Skill"] = [] #Create lsit name skill
+
+# ===================================
+# PLAYER CLASS
+# ===================================
+'''
+Create class by use inheritance
+-Warrior 
+-Mage
+'''
+
+class Warrior(Character):
+    pass
+
+class Mage(Character):
+    pass
+
+# ===================================
+# ENEMY SYSTEM
+# ===================================
+class Enemy(Character):
+    pass
+
+# ===================================
+# SKILL SYSTEM
+# ===================================
+
+class Skill(ABC):
+    pass
+
+class Slash(Skill):
+    pass
+
+class Fireball(Skill):
+    pass
+
+# ===================================
+# ITEM SYSTEM & INVENTORY SYSTEM
+# ===================================
+
+class Item(ABC):
+    pass
+
+class Potions(Item):
+    pass
+
+class Weapons(Item):
+    pass
+
+
+class Inventory:
+    pass
+
+# ===================================
+# BATTLE SYSTEM
+# ===================================
+
+class Battle:
+    pass
+
+ # ===================================
+ # OUTPUT
+ # ===================================
+
+ 
