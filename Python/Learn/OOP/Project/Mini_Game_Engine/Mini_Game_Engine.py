6a585d052917fca7953d7cbb40643f54fb74c4b4
Supatcharanan <supatcharanan.thanimluk@gmail.com>
2026-04-02 19:59:05 +0700

    Update OOP project
    Edit skill , item , inventory , character class

--- Python/Learn/OOP/Project/Mini_Game_Engine/Mini_Game_Engine.py
+++ Python/Learn/OOP/Project/Mini_Game_Engine/Mini_Game_Engine.py
@@ -22,14 +22,10 @@ class Character(ABC):
         #---List_attribute---
         self.skill:list["Skill"] = [] #Create lsit name skill
 
-    #---Abstract Method---
-    @abstractmethod
-    def basic_attack(self , target):
-        pass
-
-    @abstractmethod
-    def take_damage(self , damage):
-        pass
+    def take_damage(self , damage: int) -> int:
+        actual = max(1, damage - self.defense)
+        self.hp = max(0, self.hp - actual)
+        return actual
 
     #---Function---
     def is_alive(self) -> bool:
@@ -69,10 +65,21 @@ class Warrior(Character):
     def __init__(self, name: str):
         super().__init__(name, hp=150, mp=30, attack=25, defense=15)
         self.rage = 0
-        self.skill = [Slash]
+        self.skill = [Slash()]
+
+    def take_damage(self, damage: int) -> int:
+        actual = super().take_damage(damage)
+        self.rage = min(100 , self.rage + actual // 5)
+        return actual
+
 
 class Mage(Character):
-    pass
+    def __init__(self, name: str):
+        super().__init__(name, hp=80, mp=120, attack=10, defense=5)
+        self.spell_power = 30
+        self.skill = [Fireball()]
+
+
 
 # ===================================
 # ENEMY SYSTEM
@@ -85,30 +92,89 @@ class Enemy(Character):
 # ===================================
 
 class Skill(ABC):
-    pass
+    def __init__(self , name: str, mp_cost: int, description: str):
+        self.name = name
+        self.mp_cost = mp_cost
+        self.description = description
 
+    @abstractmethod
+    def activate(self, user: Character, target: Character) -> str:
+        pass
+        
 class Slash(Skill):
-    pass
+    def __init__(self, name, mp_cost, description):
+        super().__init__("Slash", 5, "Make more damage")
+
+    def activate(self, user, target):
+        if user.mp < self.mp_cost:
+            return "Mp isn't enough!"
+        user.mp -= self.mp_cost
+        actual = target.take_damage(user.attack + 10)
+        return f"{user.name} use {self.name}! deal {actual} damage"
 
 class Fireball(Skill):
-    pass
-
+    def __init__(self, name, mp_cost, description):
+        super().__init__("Fireball", 20, "Fire magic , single damage")
+    
+    def activate(self, user: Mage, target):
+        if user.mp < self.mp_cost:
+            return "Mp isn't enough!"
+        user.mp -= self.mp_cost
+        dmg = user.spell_power * 2
+        actual = target.take_damage(dmg)
+        return f"{user.name} use {self.name}! deal {actual} magic damge"
 # ===================================
 # ITEM SYSTEM & INVENTORY SYSTEM
 # ===================================
 
 class Item(ABC):
-    pass
+    def __init__(self, name: str, description: str):
+        self.name = name
+        self.description = description
 
-class Potions(Item):
-    pass
+    @abstractmethod
+    def use(self, target: Character) -> str:
+        pass
 
-class Weapons(Item):
-    pass
+class HealthPotion(Item):
+    def __init__(self, heal_amount: 50):
+        super().__init__("Health Potion", f"heal Hp {heal_amount}")
+        self.heal_amount = heal_amount
+
+    def use(self, target: Character) -> str:
+        actual = target.heal(self.heal_amount)
+        return f"{target.name} use {self.name} heal {actual} Hp"
+        
+class ManaPotion(Item):
+    def __init__(self, Mana_amount: 40):
+        super().__init__("Mana Potion", f"Increase mana {Mana_amount}")
+        self.Mana_amount = Mana_amount
+
+    def use(self, target: Character):
+        actual = min(self.Mana_amount, target.max_mp - target.mp)
+        target.mp += actual
+        return f"{target.name} use {self.name} increaes {actual} mp"
 
 
 class Inventory:
-    pass
+    def __init__(self):
+        self.item: list[Item] = [] #Only item in Item class can in this list
+
+    def add(self, item: Item):
+        self.item.append(item)
+        print(f"add {item.name} to your inventory")
+
+    def use(self, index: int, target: Character) -> str:
+        if index >= len(self.item):
+            return "You not have this item!"
+        item = self.item.pop(index) #delete lsit want to delete (index of item)
+        return item.use(target)
+    
+    def show(self):
+        if not self.item:
+            print("Your inventory is empty!")
+        for i , item in enumerate(self.item):
+            print(f"[{i}] {item.name} - {item.description}")
 
 # ===================================
 # BATTLE SYSTEM
