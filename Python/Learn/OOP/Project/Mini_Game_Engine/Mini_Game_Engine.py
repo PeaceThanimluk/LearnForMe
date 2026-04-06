eeb33675567b1ea86b426b91e22e82d8f60070b5
Supatcharanan <supatcharanan.thanimluk@gmail.com>
2026-04-06 16:54:46 +0700

    Update MiniGame oop project
    add gain_exp detail
    fix level_up method(add self.exp =0)
    add use_skill method
    add ai_action method detail
    add battle class
    use output
    
    special_attack(Not_Finish)

--- Python/Learn/OOP/Project/Mini_Game_Engine/Mini_Game_Engine.py
+++ Python/Learn/OOP/Project/Mini_Game_Engine/Mini_Game_Engine.py
@@ -41,16 +41,24 @@ class Character(ABC):
         return actual
 
     def gain_exp(self , amount : int):
-        pass
+        self.exp += amount
+        if self.exp >= self.level *100:
+            self.level_up()
 
     def level_up(self):
         self.level += 1
+        self.exp = 0
         self.max_hp += 20
         self.hp = self.max_hp
         self.attack += 5
         self.defense += 3
         print(f"{self.name} level up! , reach to Level.{self.level}")
 
+    def use_skill(self ,skill_index: int ,target: Character) -> str:
+        if skill_index >= len(self.skill):
+            return "There's no skill "
+        skills = self.skill[skill_index]
+        return skills.activate(self, target)
 # ===================================
 # PLAYER CLASS
 # ===================================
@@ -100,7 +108,9 @@ class Enemy(Character):
         actual = target.take_damage(dmg)
         return f"{self.name} use Berserk! deal {actual} damage"
     
-    
+    def ai_action(self, target: Character) -> str:
+        dmg = target.take_damage(self.attack + random.randint(-3, 5))
+        return f"{self.name} attack {target.name} deal {dmg} damage"
 
 # ===================================
 # SKILL SYSTEM
@@ -170,7 +180,6 @@ class ManaPotion(Item):
         target.mp += actual
         return f"{target.name} use {self.name} increaes {actual} mp"
 
-
 class Inventory:
     def __init__(self):
         self.item: list[Item] = [] #Only item in Item class can in this list
@@ -196,10 +205,90 @@ class Inventory:
 # ===================================
 
 class Battle:
-    pass
+    def __init__(self, player: Character, enemy: Enemy):
+        self.player = player
+        self.enemy = enemy
+        self.turn = 1
+
+    def run(self):
+        print(f"\n{'='*50}")
+        print(f"Battle begin!")
+        print(f"{self.player}")
+        print(f"{self.enemy}")
+        print(f"{'='*50}\n")
+
+        while self.player.is_alive() and self.enemy.is_alive():
+            print(f"--- turn {self.turn} ---")
+            self._player_turn()
+            if not self.enemy.is_alive():
+                break
+            self._enemy_turn()
+            self.turn += 1
+            print() #Empty space
+
+        self._end_battle()
+            
+
+    def _player_turn(self):
+        print(f"\n[player] Hp:{self.player.hp} MP:{self.player.mp}")
+        print("1) attack 2)skill 3)special 4)item")
+        choice = input("Choose: ").strip() #return choice to string
+
+        if choice == "1":
+            dmg = self.enemy.take_damage(self.player.attack)
+            print(f"{self.player.name} attack! deal {dmg} damage")
+        elif choice == "2":
+            if self.player.skill:
+                for i, skills in enumerate(self.player.skill):
+                    print(f"[{i}] {skills.name} (MP:{skills.mp_cost}) - {skills.description}")
+                skill_input = int(input("Choose skill: "))
+                print(" " , self.player.use_skill(skill_input, self.enemy))
+            else:
+                print("No skill")
+
+        elif choice == "3":
+            print("Special ability if coming soon!")
+        elif choice == "4":
+            self.player.inventory.show()
+            Inventory_Index_Input = input("Choose item(number): ")
+            if Inventory_Index_Input.isdigit(): #Check if input is only number (return to bool)
+                print(" ", self.player.inventory.use(int(Inventory_Index_Input) , self.player))
+        
+        else:
+             print("skip turn")
+    
+        print(f"[Enemy] HP:{self.enemy.hp} / {self.enemy.max_hp}")
+
+    def _enemy_turn(self):
+        result = self.enemy.ai_action(self.player)
+        print(f"{result}")
+        print(f"[Player] Hp:{self.player.hp} / {self.player.max_hp}")
+
+    def _end_battle(self):
+        print(f"\n{'='*50}")
+        if self.player.is_alive():
+            print(f"{self.player.name} win!")
+            self.player.gain_exp(self.enemy.exp_reward)
+            print(f"You gain {self.enemy.exp_reward} EXP")
+        else:
+            print(f"{self.player.name} Loose!")
+        print(f"{'='*50}\n")
 
  # ===================================
  # OUTPUT
  # ===================================
+
+if __name__ == "__main__":
+    #Create player character
+    hero = Warrior("Arthur")
+    hero.inventory.add(HealthPotion(60))
+    hero.inventory.add(ManaPotion(30))
+
+    #Create Enemy Character
+    goblin_king = Enemy("Goblin King", hp=80, attack=18, defense=6, exp_reward=120)
+
+    #Battle
+    battle = Battle(hero, goblin_king)
+    battle.run()
 
  
